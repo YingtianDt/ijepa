aa1d6e983bdf6057f38425f757bb0c487c72ad7f
YingtianDt <yingtian.tang@epfl.ch>
2024-05-12 16:08:43 +0200

    modify for brainscore

---
+++ setup.py
@@ -0,0 +1,14 @@
+# Minimal setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ijepa',
+    version='0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+    ]
+)
+
+
